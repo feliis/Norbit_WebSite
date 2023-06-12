c48b50477072f2e81eebf508f9a57227baf4ed77
feliis <99050400+feliis@users.noreply.github.com>
2023-06-12 13:47:36 +0300

    blya

---
+++ WebSite/WebSite/Controllers/CassetteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using WebApi.Models;
+using Newtonsoft.Json;
+
+namespace WebSite.Controllers
+{
+    public class CassetteController : Controller
+    {
+        public async Task<IActionResult> CassettePageAsync()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7022/Show/Cassette");
+            string json = await response.Content.ReadAsStringAsync();
+            Debug.WriteLine(json);
+            List<CassetteView> data = JsonConvert.DeserializeObject<List<CassetteView>>(json);
+            
+            foreach (var item in data)
+            {
+                Debug.WriteLine(item); 
+            }
+            return View(data);
+        }
+    }
+    
+}
